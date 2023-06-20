88aedccc3a2b35cd7a516a476dced01123c2fff9
pablommaciel <101663489+pablommaciel@users.noreply.github.com>
2023-06-20 11:46:57 -0300

    Update indexdragon.js

--- indexdragon.js
+++ indexdragon.js
@@ -4,12 +4,13 @@ const app = express();
 
 const connection = mysql.createConnection({
   host: '185.239.210.154',
-  user: 'u625977176_pingolbet',
-  password: 'Ppp135198000',
-  database: 'u625977176_pingolbet',
+  user: 'u625977176_pingolbetDrag',
+  password: 'Pp135198000',
+  database: 'u625977176_pingolbetDrag',
   connectTimeout: 60000,
   charset: 'utf8mb4'
 });
+
 
 connection.connect(err => {
   if (err) {
@@ -24,18 +25,18 @@ let lastReceivedId = null;
 let buffer = null;
 
 setInterval(() => {
-  console.log('Verificando atualizações...');
-  connection.query('SELECT * FROM spaceman ORDER BY ID DESC LIMIT 1', (err, rows) => {
+  //console.log('Verificando atualizações...');
+  connection.query('SELECT * FROM dragontiger ORDER BY ID DESC LIMIT 1', (err, rows) => {
     if(err) {
       console.error('Erro ao consultar o banco de dados: ', err);
       return;
     }
 
-    console.log('Dados recebidos do Banco de Dados.');
+    //console.log('Dados recebidos do Banco de Dados.');
 
     // Se é a primeira execução ou se o ID mudou
     if(lastReceivedId === null || rows[0].id !== lastReceivedId) {
-      console.log('Novos dados recebidos:');
+      //console.log('Novos dados recebidos:');
       
       let values = [];
       for (let key in rows[0]) {
@@ -43,19 +44,19 @@ setInterval(() => {
       }
       
       // Atualizando a variável buffer com os novos dados
-      buffer = values.join(' ');
+      buffer = values.join(', ');
 
       lastReceivedId = rows[0].id;
     } else {
-      console.log('Nenhum novo dado recebido.');
+      //console.log('Nenhum novo dado recebido.');
     }
   });
 }, 3000);  // Executa a cada 3 segundos
 
-app.get('/space-results', (req, res) => {
-  res.json({ results: buffer });
+app.get('/game-results', (req, res) => {
+  res.json({ results: `[` + buffer + `]`});
 });
 
-app.listen(3002, () => {
-  console.log('App ouvindo na porta 3000');
+app.listen(3003, () => {
+  console.log('App ouvindo na porta 3003');
 });
